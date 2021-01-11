19e1384d9cd32c811db5ed795651e9837fc7530c
Beaver-cyber <72715689+Beaver-cyber@users.noreply.github.com>
2021-01-10 19:45:43 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,94 +1,95 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
-var rope1,rope2,rope3, rope4,rope5;
-var world;
+
+var ground, stand;
+var sling, polygon, box;
+var score = 0;
 
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  var canvas = createCanvas(800,400);
 
-
-	engine = Engine.create();
-	world = engine.world;
-
-	roofObject=new roof(width/2,height/4,width/7,20);
-
-	bobDiameter=40;
-
-	startBobPositionX=600;
-	startBobPositionY=650;
-	bobObject1=new bob(600,650,40);
-	bobObject2=new bob(650,650,40);
-	bobObject3=new bob(700,650,40);
-	bobObject4=new bob(750,650,40);
-	bobObject5=new bob(800,650,40);
-	
-	
-	//Create a Ground
-	
-
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
-
-
-	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
-
-	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
-	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
-	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
-	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
-
-	Engine.run(engine);
-	//Render.run(render);
   
-}
+  engine = Engine.create();
+    world = engine.world;
 
+    polygon = Bodies.circle(50,200,20)
+  
+
+  ground = new Ground(400,height,800,20);
+
+  stand = new Ground(500,220,150,20);
+
+  box1 = new Box(520,185,30,40);
+  box2 = new Box(520,215,30,40);
+  box3 = new Box(520,245,30,40);
+  box4 = new Box(520,275,30,40);
+  box5 = new Box(520,305,30,40);
+
+  sling = new Sling(polygon.body,{x:200, y:50});
+}
+  
+  
 
 function draw() {
-  rectMode(CENTER);
-  background(230);
-  roofObject.display();
+  background(255,255,255); 
+  // review 
+  if(backgroundImg)
+  background(backgroundImg);
 
-  rope1.display()
-  rope2.display()
-  rope3.display()
-  rope4.display()
-  rope5.display()	
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
- 
+  noStroke();
+  textSize(35)
+  fill("white")
+  text("Score  " + score, width-300, 50)
+
+Engine.update(engine);
+  ground.display(); 
+  stand.display(); 
+  box1.display(); 
+  box1.score();
+  box2.display(); 
+  box2.score();
+  box3.display(); 
+  box3.score();
+  box4.display(); 
+  box4.score();
+  box5.display(); 
+  box5.score();
+
+
+  drawSprites();
+
+}
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
-function keyPressed() {
-  	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
-
-  	}
+function mouseReleased(){
+  sling.fly();
 }
 
+function keyPressed(){
+  if(keyCode === 32){
+      slingshot.attach(bird.body);
+  }
+}
+async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+  // review 
 
-
-
-
-
-
-
-
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=06 && hour<=19){
+      bg = "sprites/bg.png";
+  }
+  else{
+      bg = "sprites/bg2.jpg";
+  }
+// review 
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
